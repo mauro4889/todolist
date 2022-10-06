7f70d5a00b3ac64aaddc267f35d9e57baf7b584b
Mauro Spinello <mauro4889@gmail.com>
2022-10-06 11:00:17 -0300

    Se corrigio formulario de creacion de cuenta

--- src/pages/CreateAcount/CreateAcount.jsx
+++ src/pages/CreateAcount/CreateAcount.jsx
@@ -1,65 +1,77 @@
-import { Box, Button, FormControl, Input, Stack, Text } from '@chakra-ui/react'
+import { Box, Button, FormControl, Input, Stack, Text, VStack } from '@chakra-ui/react'
 import React from 'react'
 
 export const CreateAcount = () => {
+
+    const formStyle = {
+        width: '90%',
+        minHeight: '80%'        
+    }
+
     return (
         <Stack
             bg='white'
-            maxW='80vw'
-            h='40vh'
+            maxW='60vw'
+            minH='40vh'
+            maxH='60vh'
             margin='auto'
             align='center'
+            justify='center'
             mt='10%'
-            gap='7%'
             borderRadius='5px'>
             <Text
-            fontWeight='bold'
-            textAlign='center'
+                fontWeight='bold'
+                textAlign='center'
             >
                 Create tu cuenta para guardar tus tareas
             </Text>
-            <FormControl w='90%'>
-                <Input
-                    placeholder='Ingrese nombre'
-                    border='none'
-                    borderBottom='1px solid black'
-                    w='100%'
-                />
-            </FormControl>
-            <FormControl w='90%'>
-                <Input
-                    placeholder='Ingrese email'
-                    type='email'
-                    border='none'
-                    borderBottom='1px solid black'
-                    w='100%'
-                />
-            </FormControl>
-            <FormControl w='90%'>
-                <Input
-                    placeholder='Ingrese contraseña'
-                    type='password'
-                    border='none'
-                    borderBottom='1px solid black'
-                    w='100%'
-                />
-            </FormControl>
-            <Box>
-                <Button
-                    m='auto'
-                    h='2em'
-                    w='10em'
-                    border='1px solid #FF5A79'
-                    borderRadius='5%'
-                    p='3%'
-                    bg='white'
-                    color='#FF5A79'
-                    fontWeight='bold'
-                    cursor='pointer'
-                    _hover={{bg:'#FF5A79', color:'white'}}
-                    type='submit'>Registrarse
-                </Button>
-            </Box>
+            <form style={formStyle}>
+                <VStack gap='15%' h='100%'>
+                    <FormControl w='90%'>
+                        <Input
+                            placeholder='Ingrese nombre'
+                            border='none'
+                            borderBottom='1px solid black'
+                            w='100%'
+                        />
+                    </FormControl>
+                    <FormControl w='90%'>
+                        <Input
+                            placeholder='Ingrese email'
+                            type='email'
+                            border='none'
+                            borderBottom='1px solid black'
+                            w='100%'
+                        />
+                    </FormControl>
+                    <FormControl w='90%'>
+                        <Input
+                            placeholder='Ingrese contraseña'
+                            type='password'
+                            border='none'
+                            borderBottom='1px solid black'
+                            w='100%'
+                        />
+                    </FormControl>
+                    <Box>
+                        <Button
+                            m='auto'
+                            h='2em'
+                            w='10em'
+                            border='1px solid #FF5A79'
+                            borderRadius='5%'
+                            p='3%'
+                            bg='white'
+                            color='#FF5A79'
+                            fontWeight='bold'
+                            cursor='pointer'
+                            _hover={{ bg: '#FF5A79', color: 'white' }}
+                            type='submit'>Registrarse
+                        </Button>
+                    </Box>
+                </VStack>
+            </form>
+
         </Stack>
     )
 }
